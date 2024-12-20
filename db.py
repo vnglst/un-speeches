33f6b13c0ecf4f07a8e652f99f3784d061ebb90a
Koen van Gilst <koen@koenvangilst.nl>
2024-12-20 18:38:39 +0100

    use ruff + uv

--- db.py
+++ db.py
@@ -4,8 +4,7 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        db_path = os.getenv(
-            "DB_PATH", "data/processed/sentiments.sqlite")
+        db_path = os.getenv("DB_PATH", "data/processed/sentiments.sqlite")
         self.conn = sqlite3.connect(db_path)
         self.cur = self.conn.cursor()
 
